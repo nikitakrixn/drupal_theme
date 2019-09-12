f28ee8f9586ac11a61750277188befa989e02830
Nikita Naydenov <krixnliza@ya.ru>
2019-09-12 16:20:39 +0600

    Поставил модуль viewslideshow -> настроил его. Обновил стиль темы.

---
+++ sites/default/files/php/twig/5d7a12823bc47_views-slideshow-controls-_l6XoLY6rkaK3HS8hxTP9YKDe4/fvFa4w8mIWFTlUtVNNnJnx0Ci-evSMn0sRt6pBId2U8.php
@@ -0,0 +1,95 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/views_slideshow/templates/views-slideshow-controls-text-pause.html.twig */
+class __TwigTemplate_9219a871a5b914389f5cd1d7ce368a041683e818d2c66c0e61a01cb1df404e0d extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 16];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 16
+        echo "<span id=\"views_slideshow_controls_text_pause_";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["vss_id"] ?? null)), "html", null, true);
+        echo "\" ";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+        echo ">
+  <a href=\"#\">";
+        // line 17
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["start_text"] ?? null)), "html", null, true);
+        echo "</a>
+</span>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/views_slideshow/templates/views-slideshow-controls-text-pause.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  62 => 17,  55 => 16,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/views_slideshow/templates/views-slideshow-controls-text-pause.html.twig", "/app/modules/contrib/views_slideshow/templates/views-slideshow-controls-text-pause.html.twig");
+    }
+}
