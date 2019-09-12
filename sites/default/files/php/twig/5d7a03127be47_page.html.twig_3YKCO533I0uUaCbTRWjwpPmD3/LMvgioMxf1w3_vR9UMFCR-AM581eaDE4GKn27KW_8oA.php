aee3278f2884371ba6366a8cb9240bfcc24bcdcf
Nikita Naydenov <krixnliza@ya.ru>
2019-09-12 14:48:37 +0600

    Добавил блоки header & footer к шаблону

---
+++ sites/default/files/php/twig/5d7a03127be47_page.html.twig_3YKCO533I0uUaCbTRWjwpPmD3/LMvgioMxf1w3_vR9UMFCR-AM581eaDE4GKn27KW_8oA.php
@@ -0,0 +1,115 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/metasonic/templates/page.html.twig */
+class __TwigTemplate_a90cef9a22303695e423d84b83ba650b8c33748222bdd6fd3ce6a7a154d7f122 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 5];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<div class=\"layout-container\">
+
+  <header class=\"header\" aria-label=\"Heading\">
+    <div class=\"container\">
+      ";
+        // line 5
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "header", [])), "html", null, true);
+        echo "
+    </div>
+  </header>
+
+  <main>
+    <div class=\"container\">
+      ";
+        // line 11
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "content", [])), "html", null, true);
+        echo "
+    </div>
+  </main>
+
+  <footer class=\"footer\">
+    <div class=\"container\">
+      ";
+        // line 17
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "footer", [])), "html", null, true);
+        echo "
+    </div>
+  </footer>
+
+
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/metasonic/templates/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  79 => 17,  70 => 11,  61 => 5,  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/metasonic/templates/page.html.twig", "/app/themes/custom/metasonic/templates/page.html.twig");
+    }
+}
